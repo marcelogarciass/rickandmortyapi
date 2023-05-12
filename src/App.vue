8791455e59e51df1bbe902a1dcdc327766e52f1c
marcelogarciass <marcelogarciass@hotmail.com>
2023-05-12 14:13:15 -0300

    botão tudo sobre mim

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,35 @@
 <template>
- <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 shadow">
-  <span class="col-span-1" v-for="character in characters" :key="character.id"> 
-    <h1>{{ character.name }}</h1>
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img :src="character.image" alt="Shoes" />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Episode</h2>
-        <p> {{ character.episode }} </p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 shadow">
+    <span class="col-span-1" v-for="character in characters" :key="character.id">
+      <h1>{{ character.name }}</h1>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <figure>
+          <img :src="character.image" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Episode</h2>
+          <p>{{ character.episode }}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary" :key="character.id" @click="redirectToBuy(character.id)">Tudo sobre mim</button>
+          </div>
         </div>
       </div>
-    </div>
-
-  </span>
-</div>
+    </span>
+  </div>
 </template>
 
 <script setup lang="ts">
 import './input.css'
 import { onMounted, ref, watchEffect } from 'vue'
-import {getCharacters} from "./services/api"
+import { getCharacters } from "./services/api"
 
-const characters = ref([{
-  image:"",
-  name:"",
-  id:"",
-  
-}])
+const characters = ref([
+  {
+    image: "",
+    name: "",
+    id: "",
+  }
+])
 
 onMounted(() => {
   getCharacters()
@@ -39,12 +39,16 @@ onMounted(() => {
         image: i.image,
         name: i.name,
         id: i.id,
-  
       }))
+      console.log(characters.value)
     })
-}) 
+})
 
 watchEffect(() => {
   console.log("Characters", characters.value)
 })
+
+const redirectToBuy = (id: string) => {
+  window.location.href = `https://rickandmortyapi.com/api/character/${id}`
+}
 </script>
